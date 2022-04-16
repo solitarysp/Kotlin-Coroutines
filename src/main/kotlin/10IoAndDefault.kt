25637d92765c1f2b9f8d382166b6c546e1a8bf04
solit <s>
2022-04-17 00:11:55 +0700

    s

--- src/main/kotlin/10IoAndDefault.kt
+++ src/main/kotlin/10IoAndDefault.kt
@@ -9,10 +9,10 @@ fun main() {
                 println("Default: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
 
             }
-        launch(Dispatchers.IO) { // Sử dụng DefaultDispatcher
+        launch(Dispatchers.IO) { // Sử dụng thread của Default
             println("4: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
         }
-        launch(Dispatchers.IO) { // Sử dụng DefaultDispatcher
+        launch(Dispatchers.IO) { // Sử dụng thread của Default
             println("5: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
         }
     }
