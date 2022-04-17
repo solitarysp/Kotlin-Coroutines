1995ba3fd5d9cbeefeb281b6005189bc3d097bc2
solit <s>
2022-04-17 09:02:32 +0700

    s

--- src/main/kotlin/10IoAndDefault.kt
+++ src/main/kotlin/10IoAndDefault.kt
@@ -3,19 +3,29 @@ import sun.plugin2.util.SystemUtil
 import java.lang.reflect.Field
 
 fun main() {
-
     runBlocking(newSingleThreadContext("root Dispatcher")) {
-            launch(Dispatchers.Default.limitedParallelism(1)) { // Sử dụng DefaultDispatcher
-                println("Default: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
+        launch(Dispatchers.Default.limitedParallelism(1)) { // Sử dụng DefaultDispatcher
+            println(
+                "Default: My context is: $coroutineContext  \n: I'm working in thread ${
+                    Thread.currentThread().hashCode()
+                } \n"
+            )
 
-            }
-        launch(Dispatchers.IO) { // Sử dụng thread của Default
-            println("4: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
         }
         launch(Dispatchers.IO) { // Sử dụng thread của Default
-            println("5: My context is: $coroutineContext  \n: I'm working in thread ${Thread.currentThread().hashCode()} \n")
+            println(
+                "4: My context is: $coroutineContext  \n: I'm working in thread ${
+                    Thread.currentThread().hashCode()
+                } \n"
+            )
+        }
+        launch(Dispatchers.IO) { // Sử dụng thread của Default
+            println(
+                "5: My context is: $coroutineContext  \n: I'm working in thread ${
+                    Thread.currentThread().hashCode()
+                } \n"
+            )
         }
     }
-
 }
 
